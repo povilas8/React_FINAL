57539fad370d5914e2c3f9942355a79f26657a80
povilas8 <84262840+povilas8@users.noreply.github.com>
2023-10-23 21:49:12 +0300

    mobile menu and logo alignment fixed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -31,114 +31,121 @@ export default function Header() {
 
   return (
     <header className=' bg-slate-200 py-2'>
-      <div className='container flex justify-between'>
+      <div className='md:hidden container flex justify-between place-items-center'>
         <img
           className='w-44 sm:w-44 md:w-48 lg:w-56'
           src='https://cdn.freebiesupply.com/logos/large/2x/blackberry-logo-png-transparent.png'
           alt='BlackBerry logo'
         />
+        <GiHamburgerMenu onClick={() => setIsMobileOpen(!isMobileOpen)} />
+      </div>
+      <div className='container flex justify-between'>
+        <img
+          className='hidden md:block w-44 sm:w-44 md:w-48 lg:w-56'
+          src='https://cdn.freebiesupply.com/logos/large/2x/blackberry-logo-png-transparent.png'
+          alt='BlackBerry logo'
+        />
 
-        <nav
-          className={`md:block ${
-            isMobileOpen
-              ? 'flex flex-col absolute top-9 bg-slate-200 right-0'
-              : 'hidden'
-          }`}
-        >
-          {!ctx.isLoggedIn && (
-            <>
-              <NavLink
-                to='/shops'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Shops
-              </NavLink>
+        <div>
+          <nav
+            className={`md:block ${
+              isMobileOpen
+                ? 'flex flex-col static top-9 bg-slate-200'
+                : 'hidden'
+            }`}
+          >
+            {!ctx.isLoggedIn && (
+              <>
+                <NavLink
+                  to='/shops'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Shops
+                </NavLink>
 
-              <NavLink
-                to={'/additem'}
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Add Item
-              </NavLink>
+                <NavLink
+                  to={'/additem'}
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Add Item
+                </NavLink>
 
-              <NavLink
-                to='/myadds'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                My Adds
-              </NavLink>
-              <NavLink
-                to='/profile'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Profile
-              </NavLink>
-              <NavLink
-                to='/login'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Login
-              </NavLink>
-              <NavLink
-                to={'/register'}
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Register
-              </NavLink>
-            </>
-          )}
+                <NavLink
+                  to='/myadds'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  My Adds
+                </NavLink>
+                <NavLink
+                  to='/profile'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Profile
+                </NavLink>
+                <NavLink
+                  to='/login'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Login
+                </NavLink>
+                <NavLink
+                  to={'/register'}
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Register
+                </NavLink>
+              </>
+            )}
 
-          {ctx.isLoggedIn && (
-            <>
-              <NavLink
-                to='/shops'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Shops
-              </NavLink>
+            {ctx.isLoggedIn && (
+              <>
+                <NavLink
+                  to='/shops'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Shops
+                </NavLink>
 
-              <NavLink
-                to={'/additem'}
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Add Item
-              </NavLink>
+                <NavLink
+                  to={'/additem'}
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Add Item
+                </NavLink>
 
-              <NavLink
-                to='/myadds'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                My Adds
-              </NavLink>
-              <NavLink
-                to='/profile'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Profile
-              </NavLink>
+                <NavLink
+                  to='/myadds'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  My Adds
+                </NavLink>
+                <NavLink
+                  to='/profile'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Profile
+                </NavLink>
 
-              <NavLink
-                onClick={handleLogout}
-                to='/logout'
-                className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
-              >
-                Logout
-              </NavLink>
+                <NavLink
+                  onClick={handleLogout}
+                  to='/logout'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Logout
+                </NavLink>
 
-              <p className='inline-block text-lg px-3'>{ctx.username}</p>
-              <img
-                className='inline-block w-12 rounded-full'
-                src={
-                  ctx.userAvatar ||
-                  'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
-                }
-                alt='user avatar icon'
-              />
-            </>
-          )}
-        </nav>
-        <div className='block md:hidden place-self-center'>
-          <GiHamburgerMenu onClick={() => setIsMobileOpen(!isMobileOpen)} />
+                <p className='inline-block text-lg px-3'>{ctx.username}</p>
+                <img
+                  className='inline-block w-12 rounded-full'
+                  src={
+                    ctx.userAvatar ||
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+                  }
+                  alt='user avatar icon'
+                />
+              </>
+            )}
+          </nav>
         </div>
       </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xxl: '1600px',
+      md: '768px',
+      sm: '300px',
+
+      ...defaultTheme.screens,
+    },
     container: {
       center: true,
       padding: '1rem',
