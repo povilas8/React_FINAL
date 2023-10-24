d54a03cf3785519da7da8d621ef71cc1d83e744a
povilas8 <84262840+povilas8@users.noreply.github.com>
2023-10-24 15:38:55 +0300

    separated mobile menu from desktop

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -31,13 +31,116 @@ export default function Header() {
 
   return (
     <header className=' bg-slate-200 py-2'>
-      <div className='md:hidden container flex justify-between place-items-center'>
+      <div className='md:hidden container'>
         <img
-          className='w-44 sm:w-44 md:w-48 lg:w-56'
+          className='w-44 sm:w-44 md:w-48 lg:w-56 inline-block'
           src='https://cdn.freebiesupply.com/logos/large/2x/blackberry-logo-png-transparent.png'
           alt='BlackBerry logo'
         />
-        <GiHamburgerMenu onClick={() => setIsMobileOpen(!isMobileOpen)} />
+        <div className='flex float-right place-items-center'>
+          <GiHamburgerMenu onClick={() => setIsMobileOpen(!isMobileOpen)} />
+        </div>
+        <div className='justify-center items-center'>
+          <nav
+            className={`md:block ${
+              isMobileOpen
+                ? 'flex flex-col top-9 bg-slate-200 justify-center items-center'
+                : 'hidden'
+            }`}
+          >
+            {!ctx.isLoggedIn && (
+              <>
+                <NavLink
+                  to='/shops'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Shops
+                </NavLink>
+
+                <NavLink
+                  to={'/additem'}
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Add Item
+                </NavLink>
+
+                <NavLink
+                  to='/myadds'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  My Adds
+                </NavLink>
+                <NavLink
+                  to='/profile'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Profile
+                </NavLink>
+                <NavLink
+                  to='/login'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Login
+                </NavLink>
+                <NavLink
+                  to={'/register'}
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Register
+                </NavLink>
+              </>
+            )}
+
+            {ctx.isLoggedIn && (
+              <>
+                <NavLink
+                  to='/shops'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Shops
+                </NavLink>
+
+                <NavLink
+                  to={'/additem'}
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Add Item
+                </NavLink>
+
+                <NavLink
+                  to='/myadds'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  My Adds
+                </NavLink>
+                <NavLink
+                  to='/profile'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Profile
+                </NavLink>
+
+                <NavLink
+                  onClick={handleLogout}
+                  to='/logout'
+                  className='hover:bg-slate-400 drop-shadow-lg px-3 py-4'
+                >
+                  Logout
+                </NavLink>
+
+                <p className='inline-block text-lg px-3'>{ctx.username}</p>
+                <img
+                  className='inline-block w-12 rounded-full'
+                  src={
+                    ctx.userAvatar ||
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+                  }
+                  alt='user avatar icon'
+                />
+              </>
+            )}
+          </nav>
+        </div>
       </div>
       <div className='container flex justify-between'>
         <img
@@ -46,14 +149,9 @@ export default function Header() {
           alt='BlackBerry logo'
         />
 
-        <div>
-          <nav
-            className={`md:block ${
-              isMobileOpen
-                ? 'flex flex-col static top-9 bg-slate-200'
-                : 'hidden'
-            }`}
-          >
+        {/* Full desktop menu but hidden in mobile */}
+        <div className='justify-center items-center'>
+          <nav className={`md:block hidden ${isMobileOpen}`}>
             {!ctx.isLoggedIn && (
               <>
                 <NavLink
